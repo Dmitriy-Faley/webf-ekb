b34038c42261800ddebd1b61c2306017c0ad1019
Focus13 <C:\Users\Focus13\AppData\Roaming\The Bat!>
2023-10-03 13:46:37 +0300

    добавлен плагин и участки кода к хеду

--- app/content/themes/gulp-theme/header.php
+++ app/content/themes/gulp-theme/header.php
@@ -6,6 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo wp_get_document_title(); ?></title>
 	<?php wp_head(); ?>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KW95QG5');</script>
+		<!-- End Google Tag Manager -->
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,9 +24,14 @@
 
 	<!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
+	<meta name="google-site-verification" content="IhfOHsuFzKJWJP5GOTqU_5DnFeJThZGgq0hXr1yRCEY" />
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KW95QG5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 	<div class="nav__menu-btn--mobile">
 		<div class="wrapper-menu">
